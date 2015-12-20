d122ba6faf7684d70d8777d6d3d3bd8664e9b8a0
knguyen6 <rkamelot6262@gmail.com>
2015-12-19 23:48:59 -0800

    added parallax

--- app/components/app.ts
+++ app/components/app.ts
@@ -7,5 +7,20 @@ import { Component, View } from 'angular2/core';
     templateUrl: './app/components/app.html'
 })
 export class App {
+	ngOnInit() {
 
+	  $('.dropdown-button').dropdown({
+	      inDuration: 300,
+	      outDuration: 225,
+	      constrain_width: true,
+	      hover: false, // Activate on hover
+	      gutter: 0, // Spacing from edge
+	      belowOrigin: true // Displays dropdown below the button
+	    }
+	  );
+
+	  $(document).ready(function(){
+      	 $('.parallax').parallax();
+      });
+	}
 }
